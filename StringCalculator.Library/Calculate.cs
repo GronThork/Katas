77b890b5d3d52849d65ef3c3d300d79922471cca
salvarez <salvarez@voxelgroup.net>
2021-11-30 20:43:36 +0100

    refactor: generalize solution for 1,X

--- StringCalculator.Library/Calculate.cs
+++ StringCalculator.Library/Calculate.cs
@@ -6,22 +6,7 @@ namespace StringCalculator.Tests
     {
         public static int Sum(string s)
         {
-            if (s == "1,10")
-            {
-                return 11;
-            }
-
-            if (s == "1,11")
-            {
-                return 12;
-            }
-
-            if (s == "1,12")
-            {
-                return 13;
-            }
-
-            return 1 + int.Parse(s[2].ToString());
+            return 1 + int.Parse(s.Split(',')[1].ToString());
         }
     }
 }
