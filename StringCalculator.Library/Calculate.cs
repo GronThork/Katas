f9f9673d0628a11096dec53d4485263bab227ad9
salvarez <salvarez@voxelgroup.net>
2021-11-30 21:50:29 +0100

    refactor: general solution

--- StringCalculator.Library/Calculate.cs
+++ StringCalculator.Library/Calculate.cs
@@ -6,17 +6,7 @@ namespace StringCalculator.Tests
     {
         public static int Sum(string s)
         {
-            if (s == "2,1")
-            {
-                return 3;
-            }
-
-            if (s == "3,1")
-            {
-                return 4;
-            }
-
-            return 1 + int.Parse(s.Split(',')[1].ToString());
+            return int.Parse(s.Split(',')[0].ToString()) + int.Parse(s.Split(',')[1].ToString());
         }
     }
 }
