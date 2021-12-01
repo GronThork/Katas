23ca1583709ded99136cddf545976aae3afced60
salvarez <salvarez@voxelgroup.net>
2021-12-01 16:35:46 +0100

    test: start at love

---
+++ TennisKataWithRaul/TennisKata.Tests/ScoringShould.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace TennisKata
+{
+    public class ScoringShould
+    {
+        [Fact]
+        public void StartAtLove()
+        {
+            ScoringSystem scoringSystem = new();
+            Assert.Equal("love",scoringSystem.Score());
+        }
+    }
+}
---
+++ TennisKataWithRaul/TennisKata/ScoringSystem.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace TennisKata
+{
+    public class ScoringSystem
+    {
+        public IEnumerable<char> Score()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
