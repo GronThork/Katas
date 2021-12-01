64122e1f2a8b4b243b791f68ee8f4f4fe8ff1550
salvarez <salvarez@voxelgroup.net>
2021-12-01 08:24:12 +0100

    feat: return 0 when the input is empty

--- StringCalculator2/StringCalculator2.Library/StringC.cs
+++ StringCalculator2/StringCalculator2.Library/StringC.cs
@@ -1,10 +1,10 @@
 namespace StringCalculator2.Library
 {
-    public class StringC
+    public static class StringC
     {
         public static int Sum(string empty)
         {
-            throw new System.NotImplementedException();
+            return 0;
         }
     }
 }
