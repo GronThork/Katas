39e8b24d2298d6820405e8882dcc81df4509b0d7
salvarez <salvarez@voxelgroup.net>
2021-12-24 11:50:24 +0100

    feat: check that is not a mine in 0,0

--- MineSweeper/Board.cs
+++ MineSweeper/Board.cs
@@ -2,13 +2,16 @@ namespace MineSweeper
 {
     public class Board
     {
+        private readonly char _field;
+
         public Board(string input)
-        { 
+        {
+            _field = input[4];
         }
 
         public bool MineIn(int p0, int p1)
         {
-            return true;
+            return _field == '*';
         }
     }
 }
