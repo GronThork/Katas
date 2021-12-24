e7b4e95fe19431f4ab3cd2a9cf3739f5f861932d
salvarez <salvarez@voxelgroup.net>
2021-12-24 11:44:57 +0100

    feat: check a mine in 0,0

--- MineSweeper/Board.cs
+++ MineSweeper/Board.cs
@@ -3,13 +3,12 @@ namespace MineSweeper
     public class Board
     {
         public Board(string input)
-        {
-            throw new System.NotImplementedException();
+        { 
         }
 
         public bool MineIn(int p0, int p1)
         {
-            throw new System.NotImplementedException();
+            return true;
         }
     }
 }
