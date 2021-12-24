a0a101185921fff53e71442d8404f03bf5d60a3c
salvarez <salvarez@voxelgroup.net>
2021-12-24 11:46:20 +0100

    test: check that is not a mine in 0,0

--- MineSweeper.Tests/MineSweeperTest.cs
+++ MineSweeper.Tests/MineSweeperTest.cs
@@ -14,5 +14,15 @@ namespace MineSweeper.Tests
             
             board.MineIn(0, 0).Should().Be(true);
         }
+
+        [Fact]
+        public void CheckThatIsNotAMineIn0_0()
+        {
+            var input = "4 4\n....\n....\n.*..\n....";
+            var board = new Board(input);
+            
+            board.MineIn(0, 0).Should().Be(false);
+        }
+
     }
 }
