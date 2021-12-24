6cfa3b261cf0d42710b1e04126972c26cc7f617f
salvarez <salvarez@voxelgroup.net>
2021-12-24 19:54:26 +0100

    test: return the correct field for a input

--- MineSweeper.Tests/GameTest.cs
+++ MineSweeper.Tests/GameTest.cs
@@ -5,6 +5,14 @@ namespace MineSweeper.Tests
 {
     public class GameTest
     {
-
+        [Fact]
+        public void ReturnTheCorrectFieldForAInput()
+        {
+            var input = "4 4\n.*..\n*.*.\n..*.\n.*..";
+            var expected = "Field #1:\n2*21\n*4*2\n24*2\n1*21";
+            var game = new Game(input);
+            
+            game.Fields().Should().Be(expected);
+        }
     }
 }
--- MineSweeper/Game.cs
+++ MineSweeper/Game.cs
@@ -2,6 +2,11 @@ namespace MineSweeper
 {
     public class Game
     {
+        public Game(string input)
+        {
+            throw new System.NotImplementedException();
+        }
+
         public string Fields()
         {
             throw new System.NotImplementedException();
