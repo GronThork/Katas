b4f6df73a54e786380a21521e899c5d8c57be9db
salvarez <salvarez@voxelgroup.net>
2021-12-24 12:20:35 +0100

    feat: check that is a mine in 1,2

--- MineSweeper/Board.cs
+++ MineSweeper/Board.cs
@@ -2,16 +2,31 @@ namespace MineSweeper
 {
     public class Board
     {
-        private readonly char _field;
+        private readonly char[,] _field;
 
         public Board(string input)
         {
-            _field = input[4];
+            var dataRows = input.Split('\n');
+            var iteration = 0;
+
+            foreach (var row in dataRows)
+            {
+                if (iteration == 0)
+                {
+                    _field = new char[row[0], row[1]];
+                }
+                else
+                {
+                    for (int i = 0; i < row.Length; i++)
+                        _field[iteration-1, i] = row[i];
+                }
+                iteration++;
+            }
         }
 
         public bool MineIn(Point point)
         {
-            return _field == '*';
+            return _field[point.X,point.Y] == '*';
         }
     }
 }
