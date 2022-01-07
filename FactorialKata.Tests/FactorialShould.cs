06273dc8d9207a5c85d14026e93b80fd0aaf4301
salvarez <salvarez@voxelgroup.net>
2022-01-07 19:47:22 +0100

    chore: create initial projects

---
+++ FactorialKata.Tests/FactorialShould.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace FactorialKata.Tests
+{
+    public class FactorialShould
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
