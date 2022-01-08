6c21027fa74755f7d8e7ede7294bd033ac4de534
salvarez <salvarez@voxelgroup.net>
2022-01-08 19:56:40 +0100

    test: put the token O in the coordinate 1,0

--- TicTacToe.Tests/TicTacToeShould.cs
+++ TicTacToe.Tests/TicTacToeShould.cs
@@ -12,5 +12,14 @@ namespace TicTacToe.Tests
             table.Put('X', new Coordinates(0, 0));
             table.GetTokenPosition(new Coordinates(0, 0)).Should().Be('X');
         }
+
+        [Fact]
+        public void PutTheTokenOInTheCoordinate10()
+        {
+            var table = new Table();
+            table.Put('O', new Coordinates(1, 0));
+            table.GetTokenPosition(new Coordinates(1, 0)).Should().Be('O');
+        }
+
     }
 }
