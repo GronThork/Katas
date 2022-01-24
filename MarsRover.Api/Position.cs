a85adbe40589118af3468dc137ae75bdc07a7bbe
salvarez <salvarez@voxelgroup.net>
2022-01-24 16:16:02 +0100

    test: move from position 0,0 north to 1,0 north

---
+++ MarsRover.Api/Position.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MarsRover.Api
+{
+    public class Position
+    {
+        public Position(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MarsRover.Api/Rover.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace MarsRover.Api
+{
+    public class Rover
+    {
+        public Rover(Position position, char direction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Move(char[] commands)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Position GetPosition()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
