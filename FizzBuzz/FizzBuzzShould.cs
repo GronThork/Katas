b7640ad0adf4a85652a9ed8b4fe1720b3cfdf7ed
salvarez <sarodriguez18@gmail.com>
2025-01-27 10:24:33 +0100

    feat: Convert4To4

--- FizzBuzz/FizzBuzzShould.cs
+++ FizzBuzz/FizzBuzzShould.cs
@@ -13,12 +13,26 @@ public class FizzBuzzShould
     {
         Assert.Equal("2", FizzBuzz.Convert(2));
     }
+    
+    [Fact]
+    public void Convert4To4()
+    {
+        Assert.Equal("4", FizzBuzz.Convert(4));
+    }
 }
 
 public static class FizzBuzz
 {
     public static string Convert(int input)
     {
-        return input == 2 ? "2" : "1";
+        if (input == 4)
+        {
+            return "4";
+        }
+        if (input == 2)
+        {
+            return "2";
+        }
+        return "1";
     }
 }
