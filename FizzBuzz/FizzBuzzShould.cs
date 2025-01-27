fe0e90a9c2f2ebe45b7ad17cc139168c25bd0530
salvarez <sarodriguez18@gmail.com>
2025-01-27 09:56:26 +0100

    chore: fix FizzBuzz.csproj

---
+++ FizzBuzz/FizzBuzzShould.cs
@@ -0,0 +1,24 @@
+namespace FizzBuzz;
+
+public class FizzBuzzShould
+{
+    [Fact]
+    public void Convert1To1()
+    {
+        Assert.Equal("1", FizzBuzz.Convert(1));
+    }
+    
+    [Fact]
+    public void Convert2To2()
+    {
+        Assert.Equal("2", FizzBuzz.Convert(2));
+    }
+}
+
+public static class FizzBuzz
+{
+    public static string Convert(int input)
+    {
+        return "1";
+    }
+}
