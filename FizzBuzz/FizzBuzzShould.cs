64fda9be005fa172568c072910a51d8ab96d3204
salvarez <sarodriguez18@gmail.com>
2025-01-28 09:07:59 +0100

    refactor: extract method MultipleOf

--- FizzBuzz/FizzBuzzShould.cs
+++ FizzBuzz/FizzBuzzShould.cs
@@ -44,15 +44,25 @@ public static class FizzBuzz
 {
     public static string Convert(int input)
     {
-        if (input % 5 == 0 && input % 3 == 0)
+        if (MultipleOf3(input) && MultipleOf5(input))
             return "FizzBuzz";
         
-        if (input % 5 == 0)
+        if (MultipleOf5(input))
             return "Buzz";
 
-        if (input % 3 == 0)
+        if (MultipleOf3(input))
             return "Fizz";
         
         return input.ToString();
+    }
+
+    private static bool MultipleOf3(int input)
+    {
+        return input % 3 == 0;
+    }
+
+    private static bool MultipleOf5(int input)
+    {
+        return input % 5 == 0;
     }
 }
