d90f53d90ca90adcaeb3f4d2727904250d58a03e
salvarez <sarodriguez18@gmail.com>
2025-01-28 09:08:35 +0100

    refactor: extract FizzBuzz.cs to a file

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,28 @@
+namespace FizzBuzz;
+
+internal static class FizzBuzz
+{
+    public static string Convert(int input)
+    {
+        if (MultipleOf3(input) && MultipleOf5(input))
+            return "FizzBuzz";
+        
+        if (MultipleOf5(input))
+            return "Buzz";
+
+        if (MultipleOf3(input))
+            return "Fizz";
+        
+        return input.ToString();
+    }
+
+    private static bool MultipleOf3(int input)
+    {
+        return input % 3 == 0;
+    }
+
+    private static bool MultipleOf5(int input)
+    {
+        return input % 5 == 0;
+    }
+}
