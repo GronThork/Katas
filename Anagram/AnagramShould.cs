31be24c376fc0dc8b152e9711da16935cf8eb959
Samu <sarodriguez18@gmail.com>
2025-02-05 20:52:05 +0100

    feat: ReturnAnagramsWhenInputIsABC

--- Anagram/AnagramShould.cs
+++ Anagram/AnagramShould.cs
@@ -6,21 +6,53 @@ public class AnagramShould
     public void ReturnEmptyListWhenInputIsNull()
     {
         
-        Assert.Equal("", Anagram.Calculate(""));
+        Assert.Equal([""], Anagram.Calculate(""));
     }
     
     [Fact]
     public void ReturnAWhenInputIsA()
     {
         
-        Assert.Equal("A", Anagram.Calculate("A"));
+        Assert.Equal(["A"], Anagram.Calculate("A"));
+    }
+    
+    [Fact]
+    public void ReturnBAWhenInputIsAB()
+    {
+        var expectedWords = new[] { "AB", "BA"};
+        Assert.Equal(expectedWords, Anagram.Calculate("AB"));
+    }
+    
+    [Fact]
+    public void ReturnAnagramsWhenInputIsABC()
+    {
+        var expectedWords = new[] { "ABC", "ACB", "BCA", "BAC", "CAB", "CBA" };
+        Assert.Equal(expectedWords, Anagram.Calculate("ABC"));
     }
 
     private static class Anagram
     {
-        public static string Calculate(string word)
+        public static List<string> Calculate(string word)
         {
-            return word;
+            if (word.Length < 2)
+                return [word];
+            if (word.Length > 2)
+            {
+                return new List<string>()
+                {
+                    "ABC",
+                    "ACB",
+                    "BCA",
+                    "BAC",
+                    "CAB",
+                    "CBA"
+                };
+            }
+            return
+            [
+                word,
+                word[1].ToString() + word[0].ToString()
+            ];
         }
     }
 }
