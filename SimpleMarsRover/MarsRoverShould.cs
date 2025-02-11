62058f437e4f11ac1bc0befeea3852d2916248df
Samu <samuel.alvarez@amadeus.com>
2025-02-11 20:34:58 +0100

    test: RotateToRightDirection

--- SimpleMarsRover/MarsRoverShould.cs
+++ SimpleMarsRover/MarsRoverShould.cs
@@ -15,6 +15,7 @@ public class MarsRoverShould
     [InlineData("R", "0:0:E")]
     [InlineData("RR", "0:0:S")]
     [InlineData("RRR", "0:0:W")]
+    [InlineData("RRRR", "0:0:N")]
     public void RotateToRightDirection(string movements, string expectedPosition)
     {
         var marsRover = new MarsRover();
