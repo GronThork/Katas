a511769a2ac11ffebf04c144199eb9f777171be8
Samu <samuel.alvarez@amadeus.com>
2025-02-15 20:17:16 +0100

    test: CountTheNumberOfRepeteadWords

--- TextProcessing/TextProcessingShould.cs
+++ TextProcessing/TextProcessingShould.cs
@@ -8,14 +8,31 @@ public class TextProcessingShould
     [InlineData("Hello, this is an example for you to practice. You should grab this text and make it as your test case.", 21)]
     public void CountTheNumberOfWords(string text, int expected)
     {
-        Assert.Equal(expected, TextProcessing.CountWords(text));
+        Assert.Equal(expected, TextProcessing.CountNumberOfWords(text));
+    }
+
+    [Fact]
+    public void CountTheNumberOfRepeteadWords()
+    {
+        var expected = new Dictionary<string, int>
+        {
+            { "Hello", 1 },
+            { "World", 1 }
+        };
+
+        Assert.Equal(expected, TextProcessing.CountRepeatedWords());
     }
 }
 
 public static class TextProcessing
 {
-    public static int CountWords(string text)
+    public static int CountNumberOfWords(string text)
     {
         return text.Split(" ").Length;
+    }
+
+    public static IEnumerable<KeyValuePair<string, int>> CountRepeatedWords()
+    {
+        return [];
     }
 }
