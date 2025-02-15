151179cb60627ea424d9452509d555e234f05e95
Samu <samuel.alvarez@amadeus.com>
2025-02-15 19:53:31 +0100

    test: CountTheNumberOfWords

--- TextProcessing/TextProcessingShould.cs
+++ TextProcessing/TextProcessingShould.cs
@@ -5,6 +5,7 @@ public class TextProcessingShould
     [Theory]
     [InlineData("Hello World", 2)]
     [InlineData("Hello im a human", 4)]
+    [InlineData("Hello, this is an example for you to practice. You should grab this text and make it as your test case.", 21)]
     public void CountTheNumberOfWords(string text, int expected)
     {
         Assert.Equal(expected, TextProcessing.CountWords(text));
@@ -15,6 +16,6 @@ public static class TextProcessing
 {
     public static int CountWords(string text)
     {
-        return 2;
+        return text.Split(" ").Length;
     }
 }
